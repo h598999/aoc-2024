404f6b975ecccf19c7204bcccd6aab798b30ac02
h598999 <h598999@stud.hvl.no>
2024-12-02 21:49:49 +0100

    Day 2 dones

--- Day1/day1.js
+++ Day1/day1.js
@@ -1,18 +1,18 @@
-const fs = require('fs');
+import { readFile } from 'fs';
 
-fs.readFile('test.txt', 'utf8', (err, data) => {
+readFile('test.txt', 'utf8', (err, data) => {
   if (err) {
     console.error('Error reading the file:', err);
     return;
   }
     /**@type{Array<String>} */
-        const lines = data.split(/\r?\n/)
+        const lines = data.split('\n');
     /**@type{String} */
         let list1 = []
     /**@type{String} */
         let list2 = []
-    lines.forEach((line) => {
-        const parts = line.trim().split(/\s+/);
+    lines.forEach((line) => { //O(n/2)
+        const parts = line.trim().split("   ");
         if (parts.length >= 2) {
             list1.push(parts[0]); 
             list2.push(parts[1]); 
@@ -20,22 +20,21 @@ fs.readFile('test.txt', 'utf8', (err, data) => {
         }
     });
 
-    let sortedList1 = list1.sort();
-    let sortedList2 = list2.sort();
+    let sortedList1 = list1.sort(); // -> O(n log n)
+    let sortedList2 = list2.sort(); // -> O(n log n);
 
-    // let count = 0;
-    // for (let i = 0; i<sortedList1.length; i++){
-    //     count += Math.abs(sortedList1[i] - sortedList2[i])
-    // }
-    // console.log(count);
-    //
     let count = 0;
+    for (let i = 0; i<sortedList1.length; i++){
+        count += Math.abs(sortedList1[i] - sortedList2[i])
+    }
+    console.log("Part1: " + count);
+    
+    count = 0;
     let j = 0;
     let similarCounter = 0;
     const map = new Map();
-    for (let i = 0; i<sortedList1.length;){
+    for (let i = 0; i<sortedList1.length;){ // -> O(n)
         if (map.has(sortedList1[i])){
-            console.log("Adding " + map.get(sortedList1[i]) + " because we know " + sortedList1[i] + " appears " + map.get(sortedList1[i])/sortedList1[i] + " times in the list");
             count += map.get(sortedList1[i]);
             i++;
         }
@@ -47,8 +46,6 @@ fs.readFile('test.txt', 'utf8', (err, data) => {
             similarCounter = 0;
         } else {
             if (similarCounter != 0){
-                console.log(sortedList1[i] + " appears " + similarCounter + " times in the right list");
-                console.log("Adding " + (similarCounter * sortedList1[i]));
                 map.set(sortedList1[i], (similarCounter * sortedList1[i]));
                 count += (similarCounter * sortedList1[i]);
             }
@@ -56,5 +53,5 @@ fs.readFile('test.txt', 'utf8', (err, data) => {
             similarCounter = 0;
         }
     }
-    console.log(count);
+    console.log("Part2: " + count);
 });
---
+++ Day2/day2.js
@@ -0,0 +1,56 @@
+import { readFile } from 'fs';
+
+readFile('input-day2.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error reading the file:', err);
+        return;
+    }
+    const lines = data.trim().split('\n');
+
+    let safeCounter = 0;
+    for (let i = 0; i<lines.length; i++){
+        let parts = lines[i].split(' ').map(Number);
+        let decreasing = parts[0] > parts[1]
+        let increment = checkReport(parts, 1, decreasing);
+        if (increment == 0){
+            safeCounter += testSubReport(parts)
+        } else{
+            safeCounter += 1;
+        }
+    }
+    console.log(safeCounter);
+
+});
+
+/**
+    *@param {Array<Number>} parts
+    *@param {Number} j 
+    *@param {Boolean}  decreasing 
+    */
+    function checkReport(parts, j, decreasing) {
+        if (j == parts.length){
+            console.log(" " + parts + " is good")
+            return true;
+        }
+        let difference = parts[j] - parts[j-1]
+
+        if ((decreasing && difference > 0) || (!decreasing && difference < 0)){
+            return 0;
+        }
+        let absDifference = Math.abs(difference)
+        if (absDifference < 1 || absDifference > 3 ||absDifference === 0){
+            return 0;
+        }
+        return checkReport(parts, (j+1), decreasing)
+    }
+
+function testSubReport(parts){
+    for (let i = 0; i < parts.length; i++) {
+        let subReport = parts.toSpliced(i, 1);
+        let decreasing = subReport[0] > subReport[1]
+        if (checkReport(subReport, 1, decreasing))
+            return true;
+    }
+    return false;
+}
+
